563de78d7b27b99c29b6d34002efb80230a038ef
Filip Sarnecki <filipsarneckiukw@gmail.com>
2026-07-22 17:10:33 +0100

    Add first REST API endpoints

--- app.py
+++ app.py
@@ -1,9 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.get("/")
 def home():
     return "Hello World!"
+
+@app.get("/users")
+def get_users():
+    users = [
+        {
+            "id": 1,
+            "name": "Filip",
+            "role": "Technical Support"
+        },
+        {
+            "id": 2,
+            "name": "Anna",
+            "role": "QA Engineer"
+        }
+    ]
+
+    return jsonify(users)
+
+@app.get("/users/<int:user_id>")
+def get_user(user_id):
+    users = [
+        {
+            "id": 1,
+            "name": "Filip",
+            "role": "Technical Support"
+        },
+        {
+            "id": 2,
+            "name": "Anna",
+            "role": "QA Engineer"
+        }
+    ]
+
+    for user in users:
+        if user["id"] == user_id:
+            return jsonify(user)
+
+    return jsonify({
+        "message": "User not found"
+    }), 404
 
 app.run()

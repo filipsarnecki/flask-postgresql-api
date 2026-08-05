2d97108890f6698d90990128afe1bec7576cb7e1
Filip Sarnecki <filipsarneckiukw@gmail.com>
2026-08-05 15:45:25 +0100

    Add database schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    price NUMERIC(10,2) NOT NULL
+);
